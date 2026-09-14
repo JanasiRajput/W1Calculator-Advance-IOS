4a9c38e847ce5e1cdf1db6cb9e7a6bde50dda344
Janasi Rajput <rajputjanasi@gmail.com>
2026-09-14 09:52:16 -0400

    Initial Commit

---
+++ W1Calculator/W1CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  W1CalculatorApp.swift
+//  W1Calculator
+//
+//  Created by Janasi Rajput on 2026-09-14.
+//
+
+import SwiftUI
+
+@main
+struct W1CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
